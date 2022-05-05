7902fe837b8a6fec99185e1784e2c67a62754a95
Leandro <leandrobaccan76@gmail.com>
2022-05-04 21:26:08 -0300

    criando todas classes de modelo

--- src/main/java/com/dio/santander/bankline/api/model/Conta.java
+++ src/main/java/com/dio/santander/bankline/api/model/Conta.java
@@ -4,4 +4,17 @@ public class Conta {
 	private Long numero;
 	private Double saldo;
 	
+	public Long getNumero() {
+		return numero;
+	}
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+	public Double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+	
 }
